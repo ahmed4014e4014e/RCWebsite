1452a919bf7fe6b5949e8023bddcf17ce5773fca
Ahmed Al Ruqaishi <ahmed4014e@gmail.com>
2026-04-21 14:07:26 +0400

    optimized image sizes

--- UcanReactApp/src/lib/themeImages.js
+++ UcanReactApp/src/lib/themeImages.js
@@ -1,14 +1,12 @@
+import gutechImage from "../assets/campus-images/gutech.jpg";
+import squImage from "../assets/campus-images/squ.jpg";
+import mcbsImage from "../assets/campus-images/mcbs.jpg";
+
 export const themeImages = {
-  heroFort:
-    "https://images.pexels.com/photos/31367507/pexels-photo-31367507.jpeg?cs=srgb&dl=pexels-yusufcelik-31367507.jpg&fm=jpg",
-  heritageFort:
-    "https://images.pexels.com/photos/7972544/pexels-photo-7972544.jpeg?cs=srgb&dl=pexels-george-pak-7972544.jpg&fm=jpg",
-  mountainFort:
-    "https://images.pexels.com/photos/7777700/pexels-photo-7777700.jpeg?cs=srgb&dl=pexels-mikhail-nilov-7777700.jpg&fm=jpg",
-  studentsLibrary:
-    "https://images.pexels.com/photos/6549913/pexels-photo-6549913.jpeg?cs=srgb&dl=pexels-tima-miroshnichenko-6549913.jpg&fm=jpg",
-  studentsGroup:
-    "https://images.pexels.com/photos/6549642/pexels-photo-6549642.jpeg?cs=srgb&dl=pexels-tima-miroshnichenko-6549642.jpg&fm=jpg",
-  studentsStudyHall:
-    "https://images.pexels.com/photos/9572464/pexels-photo-9572464.jpeg?cs=srgb&dl=pexels-tima-miroshnichenko-9572464.jpg&fm=jpg",
+  heroFort: mcbsImage,
+  heritageFort: squImage,
+  mountainFort: gutechImage,
+  studentsLibrary: mcbsImage,
+  studentsGroup: squImage,
+  studentsStudyHall: gutechImage,
 };
