f45dfb3d3fb882637a6dfa70dbc4b806dd51b8df
Ahmed Al Ruqaishi <ahmed4014e@gmail.com>
2026-04-23 13:59:41 +0400

    updated About.jsx photo

--- UcanReactApp/src/lib/themeImages.js
+++ UcanReactApp/src/lib/themeImages.js
@@ -1,11 +1,13 @@
 import gutechImage from "../assets/campus-images/gutech.jpg";
 import squImage from "../assets/campus-images/squ.jpg";
 import mcbsImage from "../assets/campus-images/mcbs.jpg";
+import dhofarUniversityImage from "../assets/campus-images/dhofar-university.jpg";
 
 export const themeImages = {
   heroFort: mcbsImage,
   heritageFort: squImage,
   mountainFort: gutechImage,
+  dhofarUniversity: dhofarUniversityImage,
   studentsLibrary: mcbsImage,
   studentsGroup: squImage,
   studentsStudyHall: gutechImage,
